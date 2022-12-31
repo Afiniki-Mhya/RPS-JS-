f2a99fc2be659ac8e0f9e5b2a465bc8b38b3743a
Afiniki-Mhya <johnafinikimhya12@gmail.com>
2022-12-31 14:08:12 +0100

    thus far

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ const computerChoiceDisplay = document.getElementById("computer");
 const playerChoiceDisplay = document.getElementById("player");
 const possibleChoices = document.querySelectorAll(".btn");
 console.log({ possibleChoices });
-p1Display = document.querySelector("#p1");
 let playerChoice;
 let computerChoice;
 let result;
 
-// let p1Score = 0;
-// let playerScore = 0;
-
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     playerChoice = e.target.id;
+    console.log({ playerChoice: e.target });
     playerChoiceDisplay.innerHTML = "Player's Choice:  " + playerChoice;
     getComputerChoice();
     getScore();
@@ -28,40 +25,36 @@ function getComputerChoice() {
 }
 
 function getScore() {
-  if(computerChoice > playerChoice) {
-   result = "You Loose";
-   console.log(result)
+  if (computerChoice > playerChoice) {
+    result = "You Loose";
+    console.log(result);
   }
-  if(computerChoice === playerChoice) {
+  if (computerChoice === playerChoice) {
     result = "Draw";
-    console.log(result)
-   }
-   if(computerChoice < playerChoice) {
-    result = "You Win";
-    console.log(result)
-   }
+    console.log(result);
   }
+  if (computerChoice < playerChoice) {
+    result = "You Win";
+    console.log(result);
+  }
+}
 
+// if (computerChoice === "rock" && playerChoice === "paper") {
+//   p1 = ++p1Score;
+// p1.textContent = p1Score;
 
-
-
-  // if (computerChoice === "rock" && playerChoice === "paper") {
-  //   p1 = ++p1Score;
-    // p1.textContent = p1Score;
- 
-  //  if(computerChoice === "paper" && playerChoice === "scissors") {
-  //   playerScore++;
-  //  }
-  //  if(computerChoice === "paper" && playerChoice === "rock") {
-  //    computerScore++;
-  //  }
-  //  if(computerChoice === "scissors" && playerChoice === "rock") {
-  //  playerScore++;
-  //  }
-  //  if(computerChoice === "scissors" && playerChoice === "paper") {
-  //   computerScore++;
-  //  }
-  //  if(computerChoice === "rock" && playerChoice === "scissors") {
-  //   computerScore++;
-  //  }
-
+//  if(computerChoice === "paper" && playerChoice === "scissors") {
+//   playerScore++;
+//  }
+//  if(computerChoice === "paper" && playerChoice === "rock") {
+//    computerScore++;
+//  }
+//  if(computerChoice === "scissors" && playerChoice === "rock") {
+//  playerScore++;
+//  }
+//  if(computerChoice === "scissors" && playerChoice === "paper") {
+//   computerScore++;
+//  }
+//  if(computerChoice === "rock" && playerChoice === "scissors") {
+//   computerScore++;
+//  }
